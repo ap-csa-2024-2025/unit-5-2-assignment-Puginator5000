858211c1655defc5da38cde11e38ce7f60595c76
Puginator5000 <emmaminime3662@gmail.com>
2024-09-21 00:34:20 -0700

    done

--- Coins.java
+++ Coins.java
@@ -2,7 +2,20 @@ public class Coins
 {
   public static void main(String[] args)
   {
-    // test code here
+    coinConverter(1234);
+  }
+
+  public static void coinConverter(int x){
+
+    System.out.println("Dollar bills: " + x / 100);
+    x %= 100;
+    System.out.println("Quarters: " + x / 25 );
+    x %= 25;
+    System.out.println("Dimes: " + x / 10);
+    x %= 10;
+    System.out.println("Nickels: " + x / 5);
+    x %= 5;
+    System.out.println("Pennies: " + x);
   }
 
   // write code for coinConverter
